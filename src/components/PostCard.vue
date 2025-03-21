4da64f5d4f785e2517fe256e14367362c7d53978
Roman <romanjolsen@gmail.com>
2025-03-21 14:54:47 -0600

    customizing posts

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -10,9 +10,33 @@ defineProps({
 
 
 <template>
-  <div>Card</div>
-  {{ postProp.body }}
+  <div class="card">
+    <div class="card-header">
+      <div class="d-flex align-items-center justify-content-between">
+        <div>
+          <img :src="postProp.Creator.picture" class="creator-photo" alt="Creator photo">
+          <span>{{ postProp.Creator.name }}</span>
+        </div>
+        <span>Posted: {{ postProp.createdAt.toLocaleDateString() }}</span>
+      </div>
+    </div>
+    <div class="card-body">
+      <p>{{ postProp.body }}</p>
+      <img :src="postProp.ImgUrl" alt="Image">
+    </div>
+    <div class="card-footer">
+
+    </div>
+  </div>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.creator-photo {
+  height: 8dvh;
+  border-radius: 50%;
+  aspect-ratio: 1/1;
+  object-fit: cover;
+
+}
+</style>
--- src/models/Post.js
+++ src/models/Post.js
@@ -1,12 +1,14 @@
 export class Post{
   constructor(data){
     this.body = data.body
-    this.ImgUrl = data.ImgUrl
+    this.ImgUrl = data.imgUrl
     this.CreatorId = data.CreatorId
     this.LikeIds = data.LikeIds
-    this.Creator = data.Creator // Virtual
+    this.Creator = data.creator // Virtual
     this.Likes = data.Likes
     this.id = data.id
+
+    this.createdAt = new Date(data.createdAt)
 
 
 
